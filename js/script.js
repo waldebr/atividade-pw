4471a264165c8c0884c9fde335eedb03fcee2199
Waldemar <149939866+waldebr@users.noreply.github.com>
2024-10-15 21:15:26 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,6 @@
+years = prompt("Digite seu ano de nascimento: ");
+
+calc = (years - 2024) * (-1)
+
+console.log(`fulano tem ${calc} anos de idade :)`)
+
---
+++ par e impar/script.js
@@ -0,0 +1,7 @@
+num = prompt("digite um número: ")
+
+if(num%2 == 1){
+    alert(`${num} é um número impar`)
+}else{
+    alert(`${num} é um número par`)
+}
